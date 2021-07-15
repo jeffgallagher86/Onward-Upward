f0cb5bb2879f29d0af2263f5f8862ba79c7df2be
jeffgallagher86 <jeffgallagher86@gmail.com>
2021-07-15 17:09:52 +0000

    Add edit and delete buttons

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
-  });
-
-  $(document).ready(function(){
     $('.modal').modal();
-  });
-
-  $(document).ready(function(){
     $('.tooltipped').tooltip();
-  });
-
-  $(document).ready(function(){
     $('select').formSelect();
   });
